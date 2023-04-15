fea50a98b30a3173eada41810efd1fb2f733fa65
kimutai geofry <kimutaigeofry048@gmail.com>
2023-04-15 17:55:55 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// JavaScript
+// Filter skills based on search input
+document.getElementById('searchInput').addEventListener('input', function() {
+    var searchValue = this.value.toLowerCase();
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+      var skillsList = card.querySelector('.skills-list');
+      var skills = skillsList.getElementsByTagName('li');
+      for (var i = 0; i < skills.length; i++) {
+        var skill = skills[i].querySelector('.skill-name').textContent.toLowerCase();
+        if (skill.indexOf(searchValue) === -1) {
+          skills[i].style.display = 'none';
+        } else {
+          skills[i].style.display = 'block';
+        }
+      }
+    });
+  });
+  
